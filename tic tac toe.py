69f6066671813c4cee10641d16c771e37834b18f
Varun Banka <88031057+VarunBanka@users.noreply.github.com>
2022-08-06 20:51:13 +0530

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,102 +1,216 @@
-class Board:
-    def __init__(self):
-        self.boardState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+import cv2
+import numpy as np
+from random import randint
+import time
 
-    def play(self):
-        turn = 1  # 1 for X and 0 for O
-        print("Welcome to Tic Tac Toe")
-        while True:
-            self.printBoard()
-            if turn == 1:
-                print("X's Chance")
-                value = self.checkInput()
-                self.boardState[value] = "X"
+class Block():
+    def __init__(self, i, j):
+        self.value = None
+        self.pos = (i, j)
+
+    def setValue(self, value):
+        self.value = value
+
+class GUI():
+    def __init__(self, windowName):
+        self.windowName = windowName
+        self.width, self.height = 500, 500
+        self.menuHeight = 100
+        self.image = np.zeros((self.height + self.menuHeight, self.width, 3), np.uint8)
+        self.turn = 1
+        self.vsCom = 0
+        self.reset()
+
+    def reset(self):
+        self.blocks = []
+        self.win = False
+        self.change = True
+        self.selected = False
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append([Block(i, j), (j * (self.width // 3) + 3, i * (self.height // 3) + 3),
+                            ((j + 1) * (self.width // 3) - 3, (i + 1) * (self.height // 3) - 3)])
+            self.blocks.append(row)
+
+
+    def draw(self):
+        self.image = np.zeros((self.height + self.menuHeight, self.width, 3), np.uint8)
+        for i in range(3):
+            for j in range(3):
+                start_point = self.blocks[i][j][1]
+                end_point = self.blocks[i][j][2]
+                cv2.rectangle(self.image, start_point, end_point, (255, 255, 255), -1)
+                value = " " if self.blocks[i][j][0].value is None else self.blocks[i][j][0].value
+                cv2.putText(self.image, value, (j * (self.width // 3) + 25, (i * self.height // 3) + 100),
+                            cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 0), 5)
+        if self.checkWin():
+            string = ("Player " + str(
+                self.turn) + " Wins" if self.turn != self.vsCom else "Computer Wins") if self.turn == 1 else (
+                "Player " + str(2) + " Win" if self.turn != self.vsCom else "Computer Win")
+        else:
+            if not self.checkDraw():
+                string = ("Player " + str(
+                    self.turn) + "'s Turn" if self.turn != self.vsCom else "Computer's Turn") if self.turn == 1 else (
+                    "Player " + str(2) + "'s Turn" if self.turn != self.vsCom else "Computer's Turn")
             else:
-                print("O's Chance")
-                value = self.checkInput()
-                self.boardState[value] = "O"
+                string = "Match Draw!!"
+        cv2.putText(self.image, string, (self.width // 2 - 70, self.height + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (255, 255, 255), 1)
+        cv2.putText(self.image, "R - Reset", (10, self.height + 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+        cv2.putText(self.image, "Esc - Exit", (10, self.height + 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+        string = "vs Computer" if self.vsCom == 0 else "vs Human"
+        cv2.putText(self.image, "Space - " + string, (self.width // 2 + 10, self.height + 80), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (255, 255, 255), 1)
 
-            cwin = self.checkWin()
-            if cwin:
-                print("Match over")
-                break
+        if self.selected and not (self.checkWin() or self.checkDraw()):
+            self.change = True
+            self.selected = False
+            self.turn *= -1
 
-            turn = 0 if turn else 1
 
-    def checkInput(self):
-        while True:
-            try:
-                value = int(input("Please enter a value: "))
-            except ValueError:
-                print("Please enter a valid value")
-                continue
-            if value > 9 or value < 0:
-                print("Please enter a valid value")
-                continue
-            break
-        return value
+    def mainLoop(self):
+        cv2.namedWindow(self.windowName)
+        cv2.setMouseCallback(self.windowName, self.mouseCall)
+        try:
+            while True and cv2.getWindowProperty(self.windowName, 1) != -1:
+                if self.change:
+                    self.change = False
+                    self.draw()
 
-    def printBoard(self):
-        print("---------")
-        for i in range(0, 9):
-            if i % 3 != 0:
-                print("|", end=" ")
-            if self.boardState[i] == "X":
-                print("X", end=" ")
-            elif self.boardState[i] == "O":
-                print("O", end=" ")
-            else:
-                print(i, end=" ")
-            if (i + 1) % 3 == 0:
-                print("\n---------")
+                    if self.vsCom == self.turn and not (self.checkWin() or self.checkDraw()):
+                        block = self.nextMove()
+                        block.setValue("x" if self.turn == 1 else "o")
+
+                        self.selected = True
+                        self.change = True
+
+                    cv2.imshow(self.windowName, self.image)
+                key = cv2.waitKey(1)
+                if key == 27:
+                    break
+                elif key == ord("r") or key == ord("R"):
+                    self.reset()
+                if key == ord(" ") and not (self.checkWin() or self.checkDraw()):
+                    if self.vsCom:
+                        self.vsCom = 0
+                    else:
+                        self.vsCom = self.turn
+                    self.change = True
+            cv2.destroyAllWindows()
+        except:
+            print("Window is successfully closed")
 
     def checkWin(self):
-        wins = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ]
+        self.win = False
+        if (self.blocks[0][0][0].value is not None and self.blocks[0][0][0].value == self.blocks[0][1][0].value ==
+            self.blocks[0][2][0].value) or (
+                self.blocks[1][0][0].value is not None and self.blocks[1][0][0].value == self.blocks[1][1][0].value ==
+                self.blocks[1][2][0].value) or (
+                self.blocks[2][0][0].value is not None and self.blocks[2][0][0].value == self.blocks[2][1][0].value ==
+                self.blocks[2][2][0].value) or (
+                self.blocks[0][0][0].value is not None and self.blocks[0][0][0].value == self.blocks[1][0][0].value ==
+                self.blocks[2][0][0].value) or (
+                self.blocks[0][1][0].value is not None and self.blocks[0][1][0].value == self.blocks[1][1][0].value ==
+                self.blocks[2][1][0].value) or (
+                self.blocks[0][2][0].value is not None and self.blocks[0][2][0].value == self.blocks[1][2][0].value ==
+                self.blocks[2][2][0].value) or (
+                self.blocks[0][0][0].value is not None and self.blocks[0][0][0].value == self.blocks[1][1][0].value ==
+                self.blocks[2][2][0].value) or (
+                self.blocks[2][0][0].value is not None and self.blocks[2][0][0].value == self.blocks[0][2][0].value ==
+                self.blocks[1][1][0].value):
+            self.win = True
+        return self.win
 
-        for win in wins:
-            if (
-                self.boardState[win[0]] == "X"
-                and self.boardState[win[1]] == "X"
-                and self.boardState[win[2]] == "X"
-            ):
-                print("X wins")
-                return True
-            elif (
-                self.boardState[win[0]] == "O"
-                and self.boardState[win[1]] == "O"
-                and self.boardState[win[2]] == "O"
-            ):
-                print("O wins")
-                return True
+    def checkDraw(self):
+        flag = True
+        for i in range(3):
+            for j in range(3):
+                if self.blocks[i][j][0].value == None:
+                    flag = False
+        return flag
 
-        if 0 not in self.boardState:
-            print("Draw")
-            return True
+    def nextMove(self):
+        flag = 0
+        blocks = []
+        for i in range(3):
+            for j in range(3):
+                if self.blocks[i][j][0].value == None:
+                    blocks.append(self.blocks[i][j][0])
+        if not (len(blocks) == sum([len(row) for row in self.blocks]) or len(blocks) == sum(
+                [len(row) for row in self.blocks]) - 1 or len(blocks) == 1):
+            scoresList = {}
+            for block in blocks:
+                if block.value == None:
+                    if self.computerWins(block):
+                        scoresList[block] = 50
+                    elif self.playerWins(block):
+                        scoresList[block] = -50
+                    elif not self.checkDraw():
+                        block.value = ("x" if self.turn == 1 else "o")
+                        scoresList[block] = self.min_max(1, self.vsCom)
+                        block.value = None
+                    else:
+                        scoresList[block] = 0
 
-        return False
+            bestScore = (
+                min(scoresList.values()) if abs(min(scoresList.values())) > abs(max(scoresList.values())) else max(
+                    scoresList.values()))
+            blocks = []
+            for block in scoresList:
+                if scoresList[block] == bestScore:
+                    blocks.append(block)
+        choice = blocks[randint(0, len(blocks) - 1)]
+
+        return choice
+
+    def min_max(self, depth, player):
+        scoresList = []
+        for row in self.blocks:
+            for block in row:
+                if block[0].value == None:
+                    if self.computerWins(block[0]):
+                        return (50 - depth)
+                    elif self.playerWins(block[0]):
+                        return (-50 + depth)
+                    else:
+                        block[0].value = ("x" if self.turn == 1 else "o")
+                        scoresList.append(self.min_max(depth + 1, player * -1))
+                        block[0].value = None
+        if scoresList:
+            return (min(scoresList) if abs(min(scoresList)) > abs(max(scoresList)) else max(scoresList))
+        return 0
+
+    def computerWins(self, block):
+        flag = False
+        block.value = ("x" if self.vsCom == 1 else "o")
+        if self.checkWin(): flag = True
+        self.win = False
+        block.value = None
+        return flag
+
+    def playerWins(self, block):
+        flag = False
+        block.value = ("x" if self.vsCom != 1 else "o")
+        if self.checkWin(): flag = True
+        self.win = False
+        block.value = None
+        return flag
+
+    def mouseCall(self, event, posx, posy, flag, param):
+        if event == cv2.EVENT_LBUTTONDOWN and not self.win and self.turn != self.vsCom:
+            self.setBlockInPos(posx, posy)
+
+    def setBlockInPos(self, x, y):
+        for i in range(3):
+            for j in range(3):
+                if self.blocks[i][j][0].value is None and self.blocks[i][j][1][0] <= x <= self.blocks[i][j][2][0] and \
+                        self.blocks[i][j][1][1] <= y <= self.blocks[i][j][2][1]:
+                    self.blocks[i][j][0].setValue("x" if self.turn == 1 else "o")
+                    self.change = True
+                    self.selected = True
+                    break
 
 
-if __name__ == "__main__":
-    Board().play()
-    while True:
-        print("Do you want to restart ? \n")
-        restart = input("y/n\n").lower()
-        if restart == "y" or restart == "n":
-            break
-
-    if restart == "y":
-        Board().play()
-
-    if restart == "n":
-        print("okay, exiting.....")
-        print("end of program")
-        exit()
+game = GUI("TicTacToe by Varun, Kusti and  Soerensen")
+game.mainLoop()
